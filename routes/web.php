b3227f41fdbd5f14f785729d0ab66b006adcdc6e
Shah Mainur Rahman <40190240+mainur123@users.noreply.github.com>
2021-06-12 13:46:35 +0600

    Template Setup

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/product_details', function () {
+    return view('product_details');
+});
+
+Route::get('/account', function () {
+    return view('account');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
